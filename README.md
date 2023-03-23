ee119e4748f7d6b4cbd1fea3c7852bfc6c4052b9
Akihiro Takamura <takamura@sbsnt.co.jp>
2023-03-23 12:44:11 +0900

    first

---
+++ README.md
@@ -0,0 +1,33 @@
+# Nextjs AWS Amplify CDK example
+
+This is an example of how to use AWS Amplify with Nextjs and CDK.
+
+## Getting Started
+
+1. Setup AWS credentials
+2. Install AWS CDK
+3. Fork this repo
+4. Install the Amplify GitHub App in your account
+   1. Open a web browser and navigate to the installation location for the Amplify GitHub App in the AWS Region where you will deploy your app. https://github.com/apps/aws-amplify-us-east-1/installations/new, Replace `REGION` with your region, e.g. `us-east-1`
+   2. Install
+   3. Add your repository
+5. Create Github Personal Access Token
+   1. with `repo` and `admin:repo_hook` permissions
+6. change the `owner` and `repository` in `infrastructure/bin/infrastructure.ts`
+7. change directory to `infrastructure`
+8. Deploy the NextJS app with `GITHUB_ACCESS_TOKEN=xxx cdk deploy`
+9. Wait a few minutes until the application is deployed, you can check the status in the AWS Amplify Console.
+   1.  if it is not working redeploy the stack manually
+
+## Note
+> Note: At the time of this post, we’ll need to run a CLI command to update the platform property of our Amplify Hosting app so that it makes use of the new hosting runtime that enables NextJS 13 features. Once deployed, the appId of our Hosting app should have been printed to the console. Use that value to run the following command in the CLI:
+> 
+> `aws amplify update-app –-app-id THE_APP_ID -–platform WEB_COMPUTE`
+
+=> this has been added to the cdk script with a `awsCustomResource` to update the app. 
+
+check: https://aws.amazon.com/de/blogs/mobile/deploy-a-nextjs-13-application-to-amplify-with-the-aws-cdk/ for updates
+## References
+
+[Blog Github Repo](https://github.com/focusOtter/cdk-fullstack-kitchen-sink/tree/with-hosting)
+[AWS Blog](https://aws.amazon.com/de/blogs/mobile/deploy-a-nextjs-13-application-to-amplify-with-the-aws-cdk/)
---
+++ infrastructure/lib/infrastructure-stack.ts
@@ -0,0 +1,131 @@
+import { CfnOutput, SecretValue, Stack, StackProps, aws_iam } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import * as codebuild from 'aws-cdk-lib/aws-codebuild'
+import * as cr from 'aws-cdk-lib/custom-resources'
+import {
+  App,
+  GitHubSourceCodeProvider,
+  RedirectStatus,
+} from '@aws-cdk/aws-amplify-alpha'
+import { CfnBranch } from 'aws-cdk-lib/aws-amplify'
+
+interface HostingStackProps extends StackProps {
+  readonly owner: string
+  readonly repository: string
+  readonly githubOauthTokenName: string
+  readonly environmentVariables?: { [name: string]: string }
+}
+
+export class AmplifyHostingStack extends Stack {
+  constructor(scope: Construct, id: string, props: HostingStackProps) {
+    super(scope, id, props)
+
+    const amplifyApp = new App(this, 'Sample', {
+      appName: 'NextJs Sample',
+      sourceCodeProvider: new GitHubSourceCodeProvider({
+        owner: props.owner,
+        repository: props.repository,
+        oauthToken: SecretValue.unsafePlainText(props.githubOauthTokenName),
+      }),
+      autoBranchDeletion: true,
+      customRules: [
+        {
+          source: '/<*>',
+          target: '	/index.html',
+          status: RedirectStatus.NOT_FOUND_REWRITE,
+        },
+      ],
+      // needed for monorepo structure
+      environmentVariables: {
+        AMPLIFY_MONOREPO_APP_ROOT: 'app',
+        AMPLIFY_DIFF_DEPLOY: 'false',
+      },
+      buildSpec: codebuild.BuildSpec.fromObjectToYaml({
+        version: '1.0',
+        // needed for monorepo structure
+        // https://docs.aws.amazon.com/amplify/latest/userguide/monorepo-configuration.html
+        applications: [
+          {
+            appRoot: 'app',
+            frontend: {
+              phases: {
+                preBuild: {
+                  commands: ['npm ci'],
+                },
+                build: {
+                  commands: ['npm run build', 'pwd', 'ls -all'],
+                },
+              },
+              artifacts: {
+                baseDirectory: '.next',
+                files: ['**/*'],
+              },
+              cache: {
+                paths: ['node_modules/**/*'],
+              },
+            },
+          },
+        ],
+      }),
+    })
+
+    const main = amplifyApp.addBranch('main', {
+      stage: 'PRODUCTION',
+      autoBuild: true,
+    })
+    // add framework manually because it is not yet supported by the Amplify CDK
+    // https://github.com/aws/aws-cdk/issues/23325
+    const cfnBranch = main.node.defaultChild as CfnBranch
+    cfnBranch.addOverride('Properties.Framework', 'Next.js - SSR')
+
+    // update platform to WEB_COMPUTE because it is not yet supported by the Amplify CDK
+    // https://aws.amazon.com/de/blogs/mobile/deploy-a-nextjs-13-application-to-amplify-with-the-aws-cdk/
+    const updatePlatform = new cr.AwsCustomResource(this, 'updatePlatform', {
+      policy: cr.AwsCustomResourcePolicy.fromStatements([
+        new aws_iam.PolicyStatement({
+          actions: ['amplify:*'],
+          resources: ['*'],
+        }),
+      ]),
+      onCreate: {
+        service: 'Amplify',
+        action: 'updateApp',
+        physicalResourceId: cr.PhysicalResourceId.of('app-update-platform'),
+        parameters: {
+          appId: amplifyApp.appId,
+          platform: 'WEB_COMPUTE',
+        },
+      },
+    })
+
+    // trigger build after stack creation
+    // https://stackoverflow.com/questions/71664346/trigger-an-aws-amplify-build-via-aws-cdk
+    const buildTrigger = new cr.AwsCustomResource(this, 'triggerAppBuild', {
+      policy: cr.AwsCustomResourcePolicy.fromStatements([
+        new aws_iam.PolicyStatement({
+          actions: ['amplify:*'],
+          resources: ['*'],
+        }),
+      ]),
+      onCreate: {
+        service: 'Amplify',
+        action: 'startJob',
+        physicalResourceId: cr.PhysicalResourceId.of('app-build-trigger'),
+        parameters: {
+          appId: amplifyApp.appId,
+          branchName: main.branchName,
+          jobType: 'RELEASE',
+          jobReason: 'Auto Start build',
+        },
+      },
+    })
+    buildTrigger.node.addDependency(updatePlatform)
+
+    new CfnOutput(this, 'appId', {
+      value: amplifyApp.appId,
+    })
+    new CfnOutput(this, 'appDomain', {
+      value: `https://${main.branchName}.${amplifyApp.appId}.amplifyapp.com`,
+    })
+  }
+}
